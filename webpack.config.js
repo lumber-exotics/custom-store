e850a0ad84b77f590894d445d78930484e4ffd26
Jonathan Peralez <jonathanperalez@Jonathans-MacBook-Pro.local>
2019-04-26 15:48:44 -0700

    add defaults

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/index.js',
+  mode: 'development',
+  devServer: {
+    publicPath: '/build/',
+    proxy: {
+      '/api': 'http://localhost:3000',
+    },
+    historyApiFallback: true,
+    // contentBase: './'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /(node_modules)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
+        }
+      }
+    }]
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  }
+};
