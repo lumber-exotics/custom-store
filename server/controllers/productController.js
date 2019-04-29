360efaa1d13eccabf68d123e48cffafd33dff341
Mike Huynh <hades@Mikes-MacBook-Pro.local>
2019-04-29 13:47:24 -0700

    connect to /cart, only inserts the first intital 6 records

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -2,14 +2,26 @@ const pool = require('../bin/postgresNode.js');
 //const productModel = require('../models/products.js')
 const productController = {};
 
-productController.createProduct = (req, res, next) => {
+productController.woodQuery = (req, res, next) => {
 
   pool.query('SELECT * FROM wood', (err, result) => {
     if (err) {
       throw err
     }
     //console.log(result);
-    res.send(result);
+    res.send(result.rows);
+  });
+  
+}
+
+productController.stainQuery = (req, res, next) => {
+
+  pool.query('SELECT * FROM stain', (err, result) => {
+    if (err) {
+      throw err
+    }
+    //console.log(result);
+    res.send(result.rows);
   });
   
 }
--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -3,15 +3,20 @@ const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController.js');
 
-router.get('/', (req, res) => {
+router.get('/wood', (req, res) => {
   //res.send('api route reached');
-  productController.createProduct(req, res);
+  productController.woodQuery(req, res);
 });
 
-router.post('/', (req, res) => {
-  console.log('api post reached');
-  console.log(req.body);
-  //productController.createUser(req, res);
+router.get('/stain', (req, res) => {
+  //res.send('api route reached');
+  productController.stainQuery(req, res);
+});
+
+router.post('/cart', (req, res) => {
+  //console.log('api post reached');
+  //console.log(req.body);
+  productController.createCart(req, res);
 });
   
 module.exports = router;
